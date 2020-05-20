1b31c063405a19677d4932c32208fdc27b8e67fa
MrT98 <toan.vp98.wapmaster@gmail.com>
2020-05-20 18:08:26 +0700

    add project

---
+++ src/CrawlNews.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CrawlNews {
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("https://dantri.com.vn/the-gioi.htm");
+
+        Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
+        scanner.useDelimiter("\\Z");
+        String content = scanner.next();
+        scanner.close();
+
+        content = content.replaceAll("\\n", "");
+
+        Pattern pattern = Pattern.compile("<img title=\"(.*?)\"");
+        Matcher matcher = pattern.matcher(content);
+        while (matcher.find()){
+            System.out.println(matcher.group(1));
+        }
+    }
+}
